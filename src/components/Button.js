da688bec1ca65c739a11cb7ed0d9fbcfc5981d0d
lanastasov <lubomir.anastasov@gmail.com>
2020-09-17 11:11:48 +0300

    typo

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
-    text-transform:apitalized;
+    text-transform:capitalize;
     font-size:1.4rem;
     background:transparent;
     border:0.05rem solid var(--lightBlue);
