9c9e31805b4eb99c5032a79e11b80f937ca5c4a1
lanastasov <lubomir.anastasov@gmail.com>
2020-09-20 14:04:49 +0300

    ProductConsumer

--- src/context.js
+++ src/context.js
@@ -27,8 +27,8 @@ class ProductProvider extends Component {
     handleDetail = () => {
         console.log('hello from detail');
     };
-    addToCart = () => {
-        console.log('hello from add to cart');
+    addToCart = (id) => {
+        console.log(`hello from add to cart.id is ${id}`);
     };
     
     render() {
