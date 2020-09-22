546e2e43b412ab1105ded93c23677f30c59ca65e
lanastasov <lubomir.anastasov@gmail.com>
2020-09-22 13:52:15 +0300

    removeItem

--- src/context.js
+++ src/context.js
@@ -76,7 +76,24 @@ class ProductProvider extends Component {
         console.log("this is decrement method");
     };
     removeItem = (id) => {
-        console.log("item removed");
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(()=>{
+            return {
+                cart: [...tempCart],
+                products: [...tempProducts]
+            }
+        }, ()=>{
+            this.addTotals();
+        })
     };
     clearCart = () => {
         // console.log("cart was cleared");
