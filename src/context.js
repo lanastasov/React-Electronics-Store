fd0a2ba1ff19874e98ac58996efe9ea89024a59e
lanastasov <lubomir.anastasov@gmail.com>
2020-09-22 14:12:21 +0300

    Quantity Decrement and Remove Item if === 0

--- src/context.js
+++ src/context.js
@@ -76,10 +76,22 @@ class ProductProvider extends Component {
         const product = tempCart[index];
         product.count = product.count + 1;
         product.total = product.count * product.price;
-        this.setState(()=>{return{cart:[...tempCart]}}, () => {this.addTotals()})
+        this.setState(()=>{return{cart:[...tempCart]}}, () => {this.addTotals()});
     };
     decrement = (id) => {
-        console.log("this is decrement method");
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        
+        product.count = product.count - 1;
+        if (product.count === 0) {
+            this.removeItem(id);
+        }
+        else {
+            product.total = product.count * product.price;
+            this.setState(()=>{return{cart:[...tempCart]}}, () => {this.addTotals()});
+        }
     };
     removeItem = (id) => {
         let tempProducts = [...this.state.products];
