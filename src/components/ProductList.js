ffb3c6ff7a8215c8fc07c73a68f6c9bb263cfbf0
lanastasov <lubomir.anastasov@gmail.com>
2020-09-22 20:51:41 +0300

    fix the style

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -13,7 +13,7 @@ export default class ProductList extends Component {
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
-                        <Title name="our" title="products"/>
+                        <Title name="IT" title="electronics"/>
                         <div className="row" >
                         <ProductConsumer>
                             {value => {
